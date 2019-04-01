2d1793e6a253fb4a0a0e819e6fcb644ef2eb0aa9
diogojhony <diogojhony@gmail.com>
2019-04-01 19:49:39 -0300

    feat(*): add component header

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './styles.css';
+
+const Header = () => (
+    <header>
+        <div className="header-content header-container">React Blog
+        </div>
+    </header>
+);
+
+export default Header;
